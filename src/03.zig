d173e81ce58e67cf0e0f2b710d71fa8b6f58445b
Nine-Tailed Fox <yugenshiro@proton.me>
2026-05-19 20:59:10 +0000

    Started solving Second Part of Day 3

--- src/03.zig
+++ src/03.zig
@@ -3,10 +3,10 @@ const std = @import("std");
 const BatteryCluster = struct {
     batteryStr: []const u8,
     allocator: std.mem.Allocator = undefined,
-    highestBatteries: []usize = undefined,
+    highestBatteries: []u8 = undefined,
 
     pub fn init(batteryCluster: []const u8, allocator: std.mem.Allocator, highestBatteriesSize: usize) !BatteryCluster {
-        const highest = try allocator.alloc(usize, highestBatteriesSize);
+        const highest = try allocator.alloc(u8, highestBatteriesSize);
         @memset(highest, 0);
         return .{ .batteryStr = batteryCluster, .allocator = allocator, .highestBatteries = highest };
     }
@@ -20,30 +20,32 @@ const BatteryCluster = struct {
     }
 };
 
-fn getHighest(batteryCluster: *BatteryCluster) void {
-    for (batteryCluster.batteryStr) |batteryVoltage| {
-        const digit = @as(usize, @intCast(batteryVoltage - '0'));
-        if (batteryCluster.highestBatteries[0] == 0) {
-            batteryCluster.highestBatteries[0] = digit;
-        } else if (batteryCluster.highestBatteries[1] == 0) {
-            batteryCluster.highestBatteries[1] = digit;
-        } else if (batteryCluster.highestBatteries[0] < batteryCluster.highestBatteries[1]) {
-            batteryCluster.highestBatteries[0] = batteryCluster.highestBatteries[1];
-            batteryCluster.highestBatteries[1] = digit;
-        } else if (batteryCluster.highestBatteries[1] < digit) {
-            batteryCluster.highestBatteries[1] = digit;
+fn getHighest(batteryCluster: *BatteryCluster) !usize {
+    for (batteryCluster.batteryStr, 0..) |batteryVoltage, batteryIndex| {
+        if (batteryIndex < batteryCluster.highestBatteries.len) {
+            batteryCluster.highestBatteries[batteryIndex] = batteryVoltage;
+            continue;
         }
+        for (batteryCluster.highestBatteries[0 .. batteryCluster.highestBatteries.len - 1], 0..) |voltage, index| {
+            if (voltage > batteryCluster.highestBatteries[index + 1]) continue;
+            @memmove(batteryCluster.highestBatteries[index .. batteryCluster.highestBatteries.len - 1], batteryCluster.highestBatteries[index + 1 ..]);
+            batteryCluster.highestBatteries[batteryCluster.highestBatteries.len - 1] = batteryVoltage;
+            break;
+        }
+        if (batteryCluster.highestBatteries[batteryCluster.highestBatteries.len - 1] < batteryVoltage) batteryCluster.highestBatteries[batteryCluster.highestBatteries.len - 1] = batteryVoltage;
     }
+    return try std.fmt.parseInt(usize, batteryCluster.highestBatteries, 10);
 }
 
-fn firstPart(content: []const u8, allocator: std.mem.Allocator, highestBatteriesSize: usize) !usize {
+fn findHighestSum(content: []const u8, allocator: std.mem.Allocator, highestBatteriesSize: usize) !usize {
     var it = std.mem.tokenizeAny(u8, content, "\n\r");
     var sum: usize = 0;
     while (it.next()) |batteryCluster| {
         var pack = try BatteryCluster.init(batteryCluster, allocator, highestBatteriesSize);
         defer pack.deinit();
-        getHighest(&pack);
-        sum += 10 * pack.highestBatteries[0] + pack.highestBatteries[1];
+        const newSum = try getHighest(&pack);
+        std.debug.print("For {s} => {d}\n", .{ batteryCluster, newSum });
+        sum += newSum;
     }
     return sum;
 }
@@ -62,9 +64,11 @@ pub fn main(init: std.process.Init) !void {
     defer arenaAllocator.free(content);
 
     std.debug.print("--- Part 1 ---\n", .{});
-    const sum = try firstPart(content, arenaAllocator);
+    var sum = try findHighestSum(content, arenaAllocator, 2);
     std.debug.print("Sum: {d}\n", .{sum});
     std.debug.print("--- Part 2 ---\n", .{});
+    sum = try findHighestSum(content, arenaAllocator, 12);
+    std.debug.print("Sum: {d}\n", .{sum});
 }
 
 test "BatterPack Init" {
@@ -72,29 +76,44 @@ test "BatterPack Init" {
 
     var pack = try BatteryCluster.init("28475992", testAllocator, 2);
     defer pack.deinit();
+    const testOneHighestBatteries = [_]u8{ 0, 0 };
     try std.testing.expectEqualStrings("28475992", pack.batteryStr);
+    try std.testing.expectEqualSlices(u8, &testOneHighestBatteries, pack.highestBatteries);
 
     pack.deinit();
-    pack = try BatteryCluster.init("222222", testAllocator, 2);
+    const testTwoHighestBatteries = [_]u8{ 0, 0, 0, 0, 0 };
+    pack = try BatteryCluster.init("222222", testAllocator, 5);
     try std.testing.expectEqualStrings("222222", pack.batteryStr);
+    try std.testing.expectEqualSlices(u8, &testTwoHighestBatteries, pack.highestBatteries);
 }
 
 test "Get Highest" {
-    var highestBatteries = [_]usize{ 0, 0 };
-    var pack = BatteryCluster{ .batteryStr = "3189", .highestBatteries = &highestBatteries };
-    getHighest(&pack);
-    try std.testing.expectEqualSlices(usize, &[_]usize{ 8, 9 }, pack.highestBatteries);
+    var testOneHighestBatteries = [_]u8{ 0, 0 };
+    var pack = BatteryCluster{ .batteryStr = "3189", .highestBatteries = &testOneHighestBatteries };
+    var number = try getHighest(&pack);
+    try std.testing.expectEqualSlices(u8, &[_]u8{ '8', '9' }, pack.highestBatteries);
+    try std.testing.expectEqual(89, number);
 
-    highestBatteries = [_]usize{ 0, 0 };
-    pack = BatteryCluster{ .batteryStr = "818181911112111", .highestBatteries = &highestBatteries };
-    getHighest(&pack);
-    try std.testing.expectEqualSlices(usize, &[_]usize{ 9, 2 }, pack.highestBatteries);
+    var testTwoHighestBatteries = [_]u8{ 0, 0, 0, 0 };
+    pack = BatteryCluster{ .batteryStr = "818181911112111", .highestBatteries = &testTwoHighestBatteries };
+    number = try getHighest(&pack);
+    try std.testing.expectEqualSlices(u8, &[_]u8{ '9', '2', '1', '1' }, pack.highestBatteries);
+    try std.testing.expectEqual(9211, number);
 }
 
 test "First Part" {
     const input = @embedFile("input/test/03.txt");
     const allocator = std.testing.allocator;
 
-    const result = try firstPart(input, allocator, 2);
+    const result = try findHighestSum(input, allocator, 2);
     try std.testing.expectEqual(357, result);
+}
+
+test "Second Part" {
+    const input = @embedFile("input/test/03.txt");
+    const allocator = std.testing.allocator;
+
+    const result = try findHighestSum(input, allocator, 12);
+
+    try std.testing.expectEqual(3121910778619, result);
 }
